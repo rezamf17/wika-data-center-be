5889893b1f29d21e0d3ae7d8da793e522879fef5
reza.muhamad <reza.muhamad@pactindo.com>
2024-02-06 17:34:37 +0700

    login api

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -5,35 +5,39 @@ const jwt = require('jsonwebtoken');
 
 const generateToken = (user, data) => {
   const payload = {
-    email: user.email,
-    id_role: data
+    nip: user.nip,
+    data : data
   };
   // console.log('user payload',data)
+  // console.log('user payload user',user)
   const token = jwt.sign(payload, 'rezabelajar', { expiresIn: '1h' });
   return token;
 };
 
 exports.loginUser = (req, res) => {
-  const { email, password } = req.body
-  Auth.loginQuery(email, (err, user) => {
-    const token = generateToken(req.body, user[0].id_role)
+  const { nip, password } = req.body
+  console.log('request body',req.body);
+  Auth.loginQuery(nip, (err, user) => {
+    // console.log('usr', user);
+    const data = [
+      {
+        nama_lengkap: user[0].nama_lengkap,
+        email: user[0].email,
+        nip: user[0].nip,
+        role_code: user[0].role_code,
+        nomor_hp: user[0].nomor_hp,
+      }
+    ]
+    const token = generateToken(req.body, data)
     bcryptjs.compare(password, user[0].password, (err, result) => {
       if (err) {
         console.error('Error comparing passwords:', err);
         return res.status(500).json({ message: 'Terjadi kesalahan saat login.' });
       }
-      if (res) {
+      if (result) {
         res.status(200).json({
           message: "Login Success",
           token: token,
-          data: [
-            {
-              nama_lengkap: user[0].nama_lengkap,
-              email: user[0].email,
-              nip: user[0].nip,
-              id_role: user[0].id_role,
-            }
-          ]
         })
       } else {
         res.status(401).json({ message: 'Username atau kata sandi salah.' });
